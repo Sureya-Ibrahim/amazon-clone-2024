f693a89f30154d570c8c67a8502e524fd4108754
Sureya-Ibrahim <146633848+Tesinu@users.noreply.github.com>
2024-02-13 21:13:53 -0500

    update on components

--- src/API/axios.js
+++ src/API/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL:"http://127.0.0.1:5001/clone-9a529/us-central1/api"
   baseURL: "https://api-th42nd76da-uc.a.run.app",
-  // baseURL: "https://api-th42nd76da-uc.a.run.app",
+   
 });
 
 export {axiosInstance}
--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,8 +1,3 @@
-// import {initializeApp} from "firebase/compat/app";
-// import {getAuth} from 'firebase/auth';
-// import 'firebase/compat/firestore'
-// import 'firebase/compat/auth'
-
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
